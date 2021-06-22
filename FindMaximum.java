8502699347573a07786b06632a4216acdbb70795
rohitdighe19 <rohitdighe47@gmail.com>
2021-06-22 23:00:09 +0530

    solution of find the maximum float

--- FindMaximum.java
+++ FindMaximum.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 //Finding maximum from given three integers
 
-public class FindMaximum {
+public class FindMaximum  {
 	  // test case 1.1 Giving Max Number at 1st position return same number
 	  // test case 1.2 Giving Max Number at 2nd position return same number
 	  // test case 1.3 Giving Max Number at 3rd position return same number
@@ -18,8 +18,23 @@ public class FindMaximum {
 	      return max; //returns largest value
 	  }
 
-	  public static void main(String args[]) {
-	      Scanner sc = new Scanner(System.in);
-	      System.out.println(testMax(10, 5, 20));
+	  //Giving three float values and finding maximum at first , second and third position.
+	  public static Float testMax(Float floatX, Float floatY, Float floatZ) {
+	      Float max = floatX;
+	      if (floatY.compareTo(max) > 0) {
+	          max = floatY;
+	      }
+	      if (floatZ.compareTo(max) > 0) {
+	          max = floatZ;
+	      }
+	      return max;
 	  }
+
+	  public static void main(String args[]) {
+	      System.out.println(testMax(10, 20, 7));
+	      System.out.println(testMax(10.2f, 12.4f, 7.8f));
+
+	  }
+	  
+	 
 	}
